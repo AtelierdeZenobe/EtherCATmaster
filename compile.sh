10061030900ac2722e2ebc435a9c9afc555188e8
Pierre-Yves Malengré <malengre.py@gmail.com>
2024-08-28 06:49:03 +0200

    more relative path in compile.sh

--- compile.sh
+++ compile.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #Modify with ethercat install dir
-EC_DIR="/home/vpu-env/ethercat"
+EC_DIR=/home/$USER/Documents/ethercat
 gcc ectest.c -o ectest\
  -I$EC_DIR/include\
  $EC_DIR/install/lib/libethercat.a
